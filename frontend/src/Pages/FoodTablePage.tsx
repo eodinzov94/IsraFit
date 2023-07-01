258b4e5bc07a9fd8ff91344782db8e0cc6b5ef98
Ron Vayner <ronv.sce@gmail.com>
2023-07-01 20:23:40 +0300

    remove global react import

--- frontend/src/Pages/FoodTablePage.tsx
+++ frontend/src/Pages/FoodTablePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { ChangeEvent, MouseEvent, useState } from "react";
 import {
   Paper,
   Table,
@@ -12,25 +12,23 @@ import {
 } from "@mui/material";
 import data from "../constants/data.json";
 
-const FoodTablePage: React.FC = () => {
+const FoodTablePage = () => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
   const [searchTerm, setSearchTerm] = useState("");
   const handleChangePage = (
-    _event: React.MouseEvent<HTMLButtonElement> | null,
+    _event: MouseEvent<HTMLButtonElement> | null,
     newPage: number
   ) => {
     setPage(newPage);
   };
 
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
 
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
     setPage(0);
   };
@@ -63,11 +61,21 @@ const FoodTablePage: React.FC = () => {
             <TableRow>
               <TableCell>Hebrew Name</TableCell>
               <TableCell>English Name</TableCell>
-              <TableCell>Protein <b>(gr)</b></TableCell>
-              <TableCell>Total Fat <b>(gr)</b></TableCell>
-              <TableCell>Carbohydrates <b>(gr)</b></TableCell>
-              <TableCell>Food Energy <b>(kcal)</b></TableCell>
-              <TableCell>Total Sugars <b>(gr)</b></TableCell>
+              <TableCell>
+                Protein <b>(gr)</b>
+              </TableCell>
+              <TableCell>
+                Total Fat <b>(gr)</b>
+              </TableCell>
+              <TableCell>
+                Carbohydrates <b>(gr)</b>
+              </TableCell>
+              <TableCell>
+                Food Energy <b>(kcal)</b>
+              </TableCell>
+              <TableCell>
+                Total Sugars <b>(gr)</b>
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
