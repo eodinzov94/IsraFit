5645a8917719b791194bc8763d37c00daf5d18d3
evgeny <eodinzov94@gmail.com>
2023-07-14 22:21:49 +0300

    UI changes in set goal page

--- frontend/src/Pages/GoalSetPage.tsx
+++ frontend/src/Pages/GoalSetPage.tsx
@@ -49,11 +49,11 @@ export default function GoalSetPage() {
 
   const formik = useFormik({
     initialValues: {
-      weightGoal:user.weight-1, dateGoal: dayjs().add(30, 'days')
+      weightGoal: user.weight - 1, dateGoal: dayjs().add(30, 'days')
     }, validationSchema: toFormikValidationSchema(GoalSchema), onSubmit: values => {
 
       // Algebra, from formulas
-      const totalCalorieDeficit = Math.abs((user.weight -values.weightGoal) * 7700)
+      const totalCalorieDeficit = Math.abs((user.weight - values.weightGoal) * 7700)
       const avgDailyDeficit = totalCalorieDeficit / values.dateGoal.diff(dayjs(), 'days')
       const TDEEDeficit = user.TDEE - avgDailyDeficit
 
@@ -69,7 +69,7 @@ export default function GoalSetPage() {
       setMessage('Please enter a valid number')
     else if (!(goal > 50 && goal < 150)) {
       setMessage('This weight is unhealthy, please reconsider!')
-    } else if (goal === user.weight ) {
+    } else if (goal === user.weight) {
       setMessage('You are already at your goal!')
     } else {
       setMessage('')
@@ -83,13 +83,13 @@ export default function GoalSetPage() {
   const checkDeficit = (TDEEDeficit: number) => {
     const lowerLimit = user.gender === 'Male' ? 1500 : 1200
     const upperLimit = user.gender === 'Male' ? 2000 : 1600
-    if(formik.values.weightGoal === user.weight) {
+    if (formik.values.weightGoal === user.weight) {
       setMessage('You are already at your goal!')
-    }else if (TDEEDeficit < lowerLimit) {
+    } else if (TDEEDeficit < lowerLimit) {
       setMessage('WARNING! This weight loss plan is too aggressive!')
     } else if (TDEEDeficit > upperLimit) {
       setMessage(`WARNING! You're risking over-eating!`)
-      
+
     } else {
       setCalories(TDEEDeficit)
     }
@@ -109,75 +109,77 @@ export default function GoalSetPage() {
       }}
     >
       {calories === 0 ? (
-          <>
-            <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
-              <FlagIcon />
-            </Avatar>
-            <Typography variant='h6' gutterBottom>
-              Ready to reach your next fitness milestone?
+        <>
+          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+            <FlagIcon />
+          </Avatar>
+          <Typography variant='h6' gutterBottom>
+            Ready to reach your next fitness milestone?
+          </Typography>
+          <Typography variant='body1' gutterBottom>
+            Tell us more about what you're trying to achieve!
+          </Typography>
+          <Divider style={{ width: '100%', marginTop: '20px' }} />
+          <Grid component='form' onSubmit={formik.handleSubmit} container spacing={3} mt={3}>
+            <Typography variant='subtitle1' gutterBottom marginLeft='20px' marginBottom='-5px'>
+              1) First, what weight are you trying to reach?
             </Typography>
-            <Typography variant='body1' gutterBottom>
-              Tell us more about what you're trying to achieve!
-            </Typography>
-            <Divider style={{ width: '100%', marginTop: '20px' }} />
-            <Grid component='form' onSubmit={formik.handleSubmit} container spacing={3} mt={3}>
-              <Typography variant='subtitle1' gutterBottom marginLeft='20px' marginBottom='-5px'>
-                1) First, what weight are you trying to reach?
-              </Typography>
-              <Grid item xs={12} container justifyContent='center'>
-                <TextField
-                  required
-                  id='weightGoal'
-                  name='weightGoal'
-                  label='Your weight goal is: (kg)'
-                  type='number'
-                  onChange={onWeightChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.weightGoal}
-                  error={(formik.touched.weightGoal && Boolean(formik.errors.weightGoal))}
-                  helperText={alertMessage}
-                  color={alertMessage.length > 0 ? 'warning' : 'primary'}
-                  FormHelperTextProps={{ sx() {
+            <Grid item xs={12} container justifyContent='center'>
+              <TextField
+                required
+                id='weightGoal'
+                name='weightGoal'
+                label='Your weight goal is: (kg)'
+                type='number'
+                onChange={onWeightChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.weightGoal}
+                error={(formik.touched.weightGoal && Boolean(formik.errors.weightGoal))}
+                helperText={alertMessage}
+                color={alertMessage.length > 0 ? 'warning' : 'primary'}
+                FormHelperTextProps={{
+                  sx() {
                     return {
                       color: 'warning.main',
                     }
-                  },}}
-                />
-              </Grid>
-              <Divider style={{ width: '96%', margin: '40px 0 20px 20px' }} />
-              <Typography variant='subtitle1' gutterBottom marginLeft='20px' marginBottom='-5px' marginTop='20px'>
-                2) Second, how long do we have?
-              </Typography>
-              <Grid item xs={12} container justifyContent='center'>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker
-                    disablePast
-                    label='Your deadline is:'
-                    value={formik.values.dateGoal}
-                    onChange={(day) => {
-                      formik.setFieldValue('dateGoal', day)
-                      setMessage('')
-                    }}
-                  />
-                </LocalizationProvider>
-              </Grid>
-              <Grid item xs={12}>
-                <Button
-                  type='submit'
-                  fullWidth
-                  variant='contained'
-                  sx={{ mt: 3, mb: 2, color: 'white' }}
-                >
-                  Ready!
-                </Button>
-              </Grid>
+                  },
+                }}
+              />
             </Grid>
-          </>)
+            <Divider style={{ width: '96%', margin: '40px 0 20px 20px' }} />
+            <Typography variant='subtitle1' gutterBottom marginLeft='20px' marginBottom='-5px' marginTop='20px'>
+              2) Second, how long do we have?
+            </Typography>
+            <Grid item xs={12} container justifyContent='center'>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  disablePast
+                  label='Your deadline is:'
+                  value={formik.values.dateGoal}
+                  onChange={(day) => {
+                    formik.setFieldValue('dateGoal', day)
+                    setMessage('')
+                  }}
+                />
+              </LocalizationProvider>
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                type='submit'
+                fullWidth
+                variant='contained'
+                sx={{ mt: 3, mb: 2, color: 'white' }}
+              >
+                Ready!
+              </Button>
+            </Grid>
+          </Grid>
+        </>)
         :
         <Grid component='form'
-              onSubmit={() => {
-                navigate('/food-table')
-              }}
+          onSubmit={() => {
+            navigate('/food-table')
+          }}
         >
           <Grid item xs={12} container justifyContent='center'>
             <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
@@ -188,33 +190,35 @@ export default function GoalSetPage() {
             {'According to our calculations, to reach your goal you should maintain a diet of: '}
           </Typography>
           <Typography align='center' color='cornflowerblue' variant='h5' gutterBottom>
-            {calories.toFixed(2)}
+            {calories.toFixed(0)}
           </Typography>
           <Typography align='center' variant='h6' gutterBottom>
             {' calories per day for the whole duration'}
           </Typography>
-          <Grid container sx={{ mt: 2, justifyContent: 'center' }} item xs={12}>
-            <Button
-              type='submit'
-              variant='text'
-              sx={{ color: 'primary.main' }}
-            >
-              Confirm and start your journey!
-            </Button>
-          </Grid>
-          <Typography align='center' sx={{ color: 'primary.main' }} variant='subtitle1' marginTop='50px' gutterBottom>
+          <Typography align='center' sx={{ color: 'warning.main' }} variant='subtitle1' marginTop='35px' gutterBottom>
             {'Unhappy with the result? let\'s try a different goal!'}
           </Typography>
-          <Grid item xs={12}>
+          <Grid container sx={{ justifyContent: 'center' }} item xs={12}>
             <Button
               type='submit'
-              fullWidth
               variant='contained'
               sx={{ mb: 2, color: 'white' }}
               onClick={() => setCalories(0)}
             >
               Go back
             </Button>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button
+              type='submit'
+              variant='contained'
+              fullWidth
+              sx={{ mb: 2, mt: 2, color: 'white' }}
+            >
+              Confirm and start your journey!
+            </Button>
+
           </Grid>
         </Grid>
       }
